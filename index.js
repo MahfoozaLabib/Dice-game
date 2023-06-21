ef4cddd15cfeab54168547669f4e919cd9459561
MahfoozaLabib <133264584+MahfoozaLabib@users.noreply.github.com>
2023-06-21 19:31:33 +0430

    Update index.js

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ if (randomNumber1 > randomNumber2){
 } else if (randomNumber2 > randomNumber1){
     document.querySelector("h1").innerHTML = " Player 2 Wins! 🚩";
 } else {
-    document.querySelector("h1").innerHTML = "equal";
+    document.querySelector("h1").innerHTML = "equal !";
 }
